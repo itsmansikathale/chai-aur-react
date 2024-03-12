9480a076b48841a48a8fe38e3f7d6c85bdaa4d03
itsmansikathale <kathalemansi@gmail.com>
2024-03-12 18:39:24 +0530

    React Router Using Project

---
+++ reactRouter/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+import React from "react";
+// import Header from './components/Header/Header'
+// import Home from "./Home/Home";
+// import Footer from './components/Footer/footer';
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+  
+    </>
+  );
+}
+
+export default App;
