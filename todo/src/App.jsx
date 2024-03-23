0476e96918fffe888b50674b9108cca1f51bd97a
itsmansikathale <kathalemansi@gmail.com>
2024-03-23 20:43:01 +0530

    Redux ToolKit Todo

--- todo/src/App.jsx
+++ todo/src/App.jsx
@@ -49,7 +49,7 @@ function App() {
       <div className="bg-[#172842] min-h-screen py-8">
         <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
           <h1 className="text-2xl font-bold text-center mb-8 mt-2">
-            Manage Your Todos
+            To-Do By Mansi
           </h1>
           <div className="mb-4">
             {/* Todo form goes here */}
